732be63572b1f1cd8f64b6250a95a9a13c096ea7
ahmedpixels <ahmedpixelspro@gmail.com>
2025-06-18 18:36:47 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+    ALIVE_IMG: process.env.ALIVE_IMG || 'https://i.postimg.cc/rFr5MTtd/Gemini-Generated-Image-13yzzy13yzzy13yz.png'
+    ALIVE_MSG: process.env.ALIVE_MSG || '*AHMED-MD is Alive!*'
 };
