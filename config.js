2263fd88b24b7c329c4942bfdd9b99b80655d152
ahmedpixels <ahmedpixelspro@gmail.com>
2025-06-22 16:37:07 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,14 @@
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+require('dotenv').config();
 
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-    ALIVE_IMG: process.env.ALIVE_IMG || 'https://i.postimg.cc/rFr5MTtd/Gemini-Generated-Image-13yzzy13yzzy13yz.png'
-    ALIVE_MSG: process.env.ALIVE_MSG || '*AHMED-MD is Alive!*'
+  ownerName: 'AHMED',
+  ownerNumber: ['923216479192'],
+  botName: 'AHMED-MD',
+  prefix: '.',
+  sessionId: process.env.SESSION_ID || '',
+  aliveImg: process.env.ALIVE_IMG || 'https://i.postimg.cc/rFr5MTtd/Gemini-Generated-Image-13yzzy13yzzy13yz.png',
+  aliveMsg:
+    process.env.ALIVE_MSG ||
+    '🤖 AHMED-MD is Alive & Running Successfully!\n\nOwner: AHMED\nBot: AHMED-MD\nChannel: https://whatsapp.com/channel/0029Vb5fvfG5vKA37OLVvI2V',
+  port: process.env.PORT || 3000,
 };
