be82f4b3375659f872e963e7ea4ead4ee4a98a94
ahmedpixels <ahmedpixelspro@gmail.com>
2026-05-11 18:24:32 +0500

    AHMED-MD

--- config.js
+++ config.js
@@ -4,6 +4,8 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID || '',
     PREFIX: process.env.PREFIX || '.',
     OWNER_NUMBER: process.env.OWNER_NUMBER || '923216479192',
+    SUDO: process.env.SUDO || '',
+    PRIVATE_MODE: process.env.PRIVATE_MODE || 'true',
     BOT_NAME: process.env.BOT_NAME || 'AHMED-MD',
     OWNER_NAME: process.env.OWNER_NAME || 'Ahmed',
 }
